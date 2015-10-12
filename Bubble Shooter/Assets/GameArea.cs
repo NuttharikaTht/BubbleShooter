b7e3c2f1caf6afa030a3f44f5a4f61dac551de22
LXYY <zhangyupkunet@gmail.com>
2015-10-11 22:09:56 -0700

    Add "GameArea" object to handle mouse event.
    When mouse is clicked in game area, a bubble will be launched.

---
+++ Bubble Shooter/Assets/GameArea.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameArea : MonoBehaviour
+{
+	public BubbleLauncher bubbleLauncher;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log ("Hello, world!");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+
+	void OnMouseDown ()
+	{
+	}
+
+	// When mouse button is released in game area, launch a new bubble.
+	void OnMouseUp ()
+	{
+		Vector3 currentMousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		bubbleLauncher.LaunchBubble (currentMousePosition - bubbleLauncher.transform.position);
+	}
+}

5ff317992772513a845c55d5f4e862af47d86ce1
LXYY <zhangyupkunet@gmail.com>
2015-10-11 17:37:51 -0700

    Initial Bubble Shooter project.
    This change implements initial project, which includes:
    - A demo scene.
    - Bubble mono behavior class.
    - A class for game board.

---
+++ Bubble Shooter/Assets/GameBoard.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+namespace BubbleShooter
+{
+	public class GameBoard
+	{
+		private float width;
+		private float height;
+		private int numBubblesEachRow;
+		private float bubbleRadius;
+
+		public GameBoard (float width, float height, int numBubblesEachRow)
+		{
+			this.width = width;
+			this.height = height;
+			this.numBubblesEachRow = numBubblesEachRow;
+			this.bubbleRadius = 0.5f * width / numBubblesEachRow;
+		}
+
+		public void snapBubble (Bubble bubble)
+		{
+		}
+
+		public void destroyBubbles (IEnumerable bubbles)
+		{
+			foreach (Bubble bubble in bubbles) {
+				bubble.Destroy ();
+			}
+		}
+
+		public void makeBubblesFall (IEnumerable bubbles)
+		{
+			foreach (Bubble bubble in bubbles) {
+				bubble.Fall ();
+			}
+		}
+
+	}
+}
+

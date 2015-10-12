6330af609a885b13cf03d450d0c63548df8b1b65
LXYY <zhangyupkunet@gmail.com>
2015-10-11 22:09:07 -0700

    Add new methods to BubbleLauncher.
    - Added LoadBubble for loading new bubble.
    - Added LaunchBubble for launching loaded bubble.

--- Bubble Shooter/Assets/BubbleLauncher.cs
+++ Bubble Shooter/Assets/BubbleLauncher.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BubbleLauncher : MonoBehaviour {
+public class BubbleLauncher : MonoBehaviour
+{
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		LoadBubble ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
+	}
+
+	// Prepare a bubble at the starting point.
+	public void LoadBubble ()
+	{
+		Debug.Log ("A new bubble is loaded.");
+	}
+
+	// Launch the bubble.
+	public void LaunchBubble (Vector3 direction)
+	{
+		Debug.LogFormat ("A new bubble is launched towards direction: {0}.", direction.normalized);
 	}
 }
